7114dfe9a1c755279ae0c5f3d577f6eadb5a1eaf
Lance <lance_wynn@cireson.com>
2016-06-22 19:01:44 -0600

    Initial Commit

---
+++ Deployment Artifacts/FixTP5NtwkBug.ps1
@@ -0,0 +1,23 @@
+﻿$KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmsmp\parameters\SwitchList"
+$keys = get-childitem $KeyPath
+foreach($key in $keys)
+{
+   if ($key.GetValue("FriendlyName") -eq 'nat')
+   {
+      $newKeyPath = $KeyPath+"\"+$key.PSChildName
+      Remove-Item -Path $newKeyPath -Recurse
+   }
+}
+remove-netnat -Confirm:$false
+Get-ContainerNetwork | Remove-ContainerNetwork -Force
+
+# Get-VmSwitch -Name nat | Remove-VmSwitch #(_failure is expected_)
+
+Stop-Service docker
+Set-Service docker -StartupType Disabled
+
+# No need to reboot host 
+
+Get-NetNat | Remove-NetNat
+Set-Service docker -StartupType automatic
+Start-Service docker
---
+++ src/SwarmNode/DeploymentArtifacts/PlatformDownload.ps1
@@ -0,0 +1,37 @@
+﻿#Function PrepPlatformContainerFolder{
+
+Param(
+[string]$path = 'c:\Cireson.Platform.Host',
+[string]$sqlServer = "vmexsqlsvri3s3ayqm2nipe",
+[string]$dbName = "CiresonPlatform",
+[string]$sqlUserName = "sqladmin",
+[string]$sqlPassword = "P@ssw0rd1!",
+[string]$version = "1.0.68-rc0008"
+)
+
+if(Test-Path $path){
+    Remove-Item $path -recurse
+}
+
+New-Item $path -ItemType Directory
+Invoke-WebRequest -Uri https://www.nuget.org/api/v2/package/Cireson.Platform.Core.Host/$version -OutFile $path\$version.nupkg
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$path\$version.nupkg", "$path\$version") 
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$path\$version\content\PlatformRuntime\Cireson.Platform.Host.zip", "$path") 
+
+
+Remove-Item $path\$version.nupkg -force
+Remove-Item $path\$version -recurse -Force
+
+$connectionString = "Server=tcp:$sqlServer.database.windows.net,1433;Data Source=$sqlServer.database.windows.net;Initial Catalog=$dbName;Persist Security Info=False;User ID=$sqlUserName;Password=$sqlPassword;Encrypt=True;Connection Timeout=30;"
+
+$configPath = "$path\Cireson.Platform.Host.exe.config"
+
+[xml]$configFile = Get-Content $configPath
+
+$cstring = (($configFile.configuration.connectionStrings).add | where {$_.name -eq "CiresonDatabase"})
+$cstring.connectionString = $connectionString
+$configFile.Save($configPath) 
+
+#}
