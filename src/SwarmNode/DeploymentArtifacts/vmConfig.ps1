f16767f5ab077af400aac75e4eb51f32620670d5
Lance <lance_wynn@cireson.com>
2016-06-24 15:47:48 -0600

    More arg fixes

--- src/SwarmNode/DeploymentArtifacts/vmConfig.ps1
+++ src/SwarmNode/DeploymentArtifacts/vmConfig.ps1
@@ -22,8 +22,9 @@ param
 
 #todo: need a way to specify an ssl cert from storage, install it locally, and set it up with the service.
 $localUserName = '.\' + $sqlUserName
+$args = "-i", "-sn", "CiresonPlatform", "-sdn", "CiresonPlatform", "-usr", $localUserName, "-pwd", $sqlPassword
 #install platform service locally, and start it running
-start-process "C:\Cireson.Platform.Host\Cireson.Platform.Host.exe" -ArgumentList "-i -sn CiresonPlatform -sdn CiresonPlatform -usr $localUserName -pwd $sqlPassword" | Out-File "C:\Cireson.Platform.Host\Cireson.Platform.Host.InstallLog.txt"
+start-process "C:\Cireson.Platform.Host\Cireson.Platform.Host.exe" -ArgumentList $args | Out-File "C:\Cireson.Platform.Host\Cireson.Platform.Host.InstallLog.txt"
 
 #todo: need to add ssl support.
 #https://azure.microsoft.com/en-us/documentation/articles/app-service-web-arm-with-msdeploy-provision/
